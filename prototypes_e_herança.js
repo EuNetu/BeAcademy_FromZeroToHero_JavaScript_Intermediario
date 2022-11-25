49c277b89eb169957d74d5cbad63e7cac8a4119b
neto <euneto2501@gmail.com>
2022-11-24 23:29:36 -0300

    Feature: aula 4 concluida

---
+++ prototypes_e_herança.js
@@ -0,0 +1,28 @@
+//Prototypes
+function User(userName, password, age) {
+  this.userName = userName;
+  this.password = password;
+  this.age = age;
+}
+
+// const camilla = new User('Milla', '2512', 19);
+// console.log(camilla.prototype)
+const user = 'Sophia'
+
+const tv = {
+  resolucao: '4k',
+  smartTV: true,
+  ano: 2020,
+  especificacoes: (price) => console.log(`Com uma resolução de ${tv.resolucao} e sendo uma tv de ${tv.ano}, por apenas ${price != null ? price: 'poucos'} reais.`)
+}
+
+tv.especificacoes()
+tv.especificacoes(3800)
+
+const lg = {
+  marca: 'LG'
+}
+
+Object.setPrototypeOf(lg, tv)
+
+lg.especificacoes(2800)
