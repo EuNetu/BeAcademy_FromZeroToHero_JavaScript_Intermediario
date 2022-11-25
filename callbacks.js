eae6ad4474b5ad4e343104333a597eca1d4b9841
neto <euneto2501@gmail.com>
2022-11-25 11:48:01 -0300

    Feature: aula 9 concluida

---
+++ callbacks.js
@@ -0,0 +1,14 @@
+//Fução de CallBack
+function hello(name) {
+  console.log(name);
+}
+
+function helloUser(n) {
+  n('Neto')
+}
+
+//helloUser(hello)
+
+const callback = e => alert('Foi clicado')
+
+window.addEventListener('click', callback)
